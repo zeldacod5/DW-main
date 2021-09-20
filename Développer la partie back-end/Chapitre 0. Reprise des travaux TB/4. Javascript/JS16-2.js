21365347edb188ad831893202c4e36f882e405d9
zeldacod5 <loucas.herault@laposte.net>
2021-09-20 16:58:10 +0200

    commit lundi

--- Développer la partie back-end/Chapitre 0. Reprise des travaux TB/4. Javascript/JS16-2.js
+++ Développer la partie back-end/Chapitre 0. Reprise des travaux TB/4. Javascript/JS16-2.js
@@ -40,3 +40,61 @@ nom.addEventListener('change', ()=>{
 prenom.addEventListener('change', ()=>{
     change(prenom,regExp1,verification) 
 });
+
+
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+
+
+document.getElementById("inscription").addEventListener("submit", function(e)
+{
+    var erreur;
+
+    var inputs = document.getElementsByTagName("input");
+
+    for (var i=0; i < inputs.length; i++) {
+        if (!inputs[i].value) {
+            erreur = "Veuillez renseigner tous les champs";
+        }
+    }
+    
+
+    if (erreur) {
+        e.preventDefault(); /*par défault change le comportement du formulaire */
+        document.getElementById("erreur").innerHTML = erreur;
+        return false; /* permet dans tout les cas de retourner une erreur si il y as erreur */
+    } else {
+        alert('Formulaire envoyé');
+    }
+});
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+
+
+document.getElementById("inscription").addEventListener("submit", function(e)
+{
+    var erreur;
+    var pseudo = document.getElementById("pseudo");
+    var email = document.getElementById("email");
+    var email2 = document.getElementById("email2");
+    var mdp = document.getElementById("mdp");
+
+        if (!mdp.value) {
+        erreur = "Veuillez renseigner un mot de passe";    }
+        if (!email.value) {
+        erreur = "Veuillez renseigner un email";
+        if (!pseudo.value) {
+        erreur = "Veuillez renseigner un pseudo";
+    }
+
+    }
+    if (erreur) {
+        e.preventDefault(); /*par défault change le comportement du formulaire */
+        document.getElementById("erreur").innerHTML = erreur;
+        return false; /* permet dans tout les cas de retourner une erreur si il y as erreur */
+    } else {
+        alert('Formulaire envoyé');
+    }
+});
